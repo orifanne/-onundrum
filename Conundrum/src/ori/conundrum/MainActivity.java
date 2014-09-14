b8920716ae5ad7b20302f03bef80ea582acf2831
orifanne <orifanne@orifanne-N130>
2014-09-14 10:57:16 +0400

    - тестовый пример OpenGL ES 2 - попытка добавить повороты камеры (пока не рабоатет)

--- Conundrum/src/ori/conundrum/MainActivity.java
+++ Conundrum/src/ori/conundrum/MainActivity.java
@@ -5,16 +5,19 @@ import java.util.TimerTask;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-@SuppressLint("DefaultLocale")
 public class MainActivity extends Activity {
 
 	TextView tvText;
@@ -33,12 +36,43 @@ public class MainActivity extends Activity {
 	float[] valuesGravity = new float[3];
 	float[] valuesGyr = new float[3];
 
+	private static final float NS2S = 1.0f / 1000000000.0f;
+	private static final float EPSILON = 1.0f / 1000000000.0f;
+	private final float[] deltaRotationVector = new float[4];
+	private float timestamp;
+	float[] deltaRotationMatrix = new float[9];
+	public static float[] rotationCurrent = new float[3];
+
+	// создадим ссылку на экземпляр нашего класса MyClassSurfaceView
+	private GLSurfaceView mGLSurfaceView;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
 
-		tvText = (TextView) findViewById(R.id.tvText);
+		mGLSurfaceView = new GLSurfaceView(this);
+
+		// Проверяем поддереживается ли OpenGL ES 2.0.
+		final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+		final ConfigurationInfo configurationInfo = activityManager
+				.getDeviceConfigurationInfo();
+		final boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000;
+
+		if (supportsEs2) {
+			// Запрос OpenGL ES 2.0 для установки контекста.
+			mGLSurfaceView.setEGLContextClientVersion(2);
+
+			// Устанавливаем рендеринг
+			mGLSurfaceView.setRenderer(new MyClassRenderer());
+			mGLSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
+		} else {
+			// Устройство поддерживает только OpenGL ES 1.x
+			// ... печаль
+			return;
+		}
+
+		setContentView(mGLSurfaceView);
+
 		sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 		sensorAccel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 		sensorLinAccel = sensorManager
@@ -61,15 +95,16 @@ public class MainActivity extends Activity {
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
+		// if (id == R.id.action_settings) {
+		// return true;
+		// }
 		return super.onOptionsItemSelected(item);
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
+		mGLSurfaceView.onResume();
 		sensorManager.registerListener(listener, sensorAccel,
 				SensorManager.SENSOR_DELAY_NORMAL);
 		sensorManager.registerListener(listener, sensorLinAccel,
@@ -86,7 +121,7 @@ public class MainActivity extends Activity {
 				runOnUiThread(new Runnable() {
 					@Override
 					public void run() {
-						showInfo();
+						// showInfo();
 					}
 				});
 			}
@@ -97,6 +132,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onPause() {
 		super.onPause();
+		mGLSurfaceView.onPause();
 		sensorManager.unregisterListener(listener);
 		timer.cancel();
 	}
@@ -143,6 +179,64 @@ public class MainActivity extends Activity {
 				for (int i = 0; i < 3; i++) {
 					valuesGyr[i] = event.values[i];
 				}
+
+				// This timestep's delta rotation to be multiplied by the
+				// current rotation
+				// after computing it from the gyro sample data.
+				if (timestamp != 0) {
+					final float dT = (event.timestamp - timestamp) * NS2S;
+					// Axis of the rotation sample, not normalized yet.
+					float axisX = event.values[0];
+					float axisY = event.values[1];
+					float axisZ = event.values[2];
+
+					// Calculate the angular speed of the sample
+					float omegaMagnitude = (float) Math.sqrt(axisX * axisX
+							+ axisY * axisY + axisZ * axisZ);
+
+					// Normalize the rotation vector if it's big enough to get
+					// the axis
+					if (omegaMagnitude > EPSILON) {
+						axisX /= omegaMagnitude;
+						axisY /= omegaMagnitude;
+						axisZ /= omegaMagnitude;
+					}
+
+					// Integrate around this axis with the angular speed by the
+					// timestep
+					// in order to get a delta rotation from this sample over
+					// the timestep
+					// We will convert this axis-angle representation of the
+					// delta rotation
+					// into a quaternion before turning it into the rotation
+					// matrix.
+					float thetaOverTwo = omegaMagnitude * dT / 2.0f;
+					float sinThetaOverTwo = (float) Math.sin(thetaOverTwo);
+					float cosThetaOverTwo = (float) Math.cos(thetaOverTwo);
+					deltaRotationVector[0] = sinThetaOverTwo * axisX;
+					deltaRotationVector[1] = sinThetaOverTwo * axisY;
+					deltaRotationVector[2] = sinThetaOverTwo * axisZ;
+					deltaRotationVector[3] = cosThetaOverTwo;
+				}
+				timestamp = event.timestamp;
+				float[] deltaRotationMatrix = new float[9];
+				SensorManager.getRotationMatrixFromVector(deltaRotationMatrix,
+						deltaRotationVector);
+				// User code should concatenate the delta rotation we computed
+				// with the current rotation
+				// in order to get the updated rotation.
+				float[] k = new float[3];
+
+				k[0] = rotationCurrent[0] * deltaRotationMatrix[0]
+						+ rotationCurrent[1] * deltaRotationMatrix[3]
+						+ rotationCurrent[2] * deltaRotationMatrix[6];
+				k[1] = rotationCurrent[0] * deltaRotationMatrix[1]
+						+ rotationCurrent[1] * deltaRotationMatrix[4]
+						+ rotationCurrent[2] * deltaRotationMatrix[7];
+				k[2] = rotationCurrent[0] * deltaRotationMatrix[2]
+						+ rotationCurrent[1] * deltaRotationMatrix[5]
+						+ rotationCurrent[2] * deltaRotationMatrix[8];
+				rotationCurrent = k;
 				break;
 			}
 
